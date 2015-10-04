06c6b4fabfc4af23a14d1e5fc340520bcd6d50ce
bmorejamie <bmorejamie@gmail.com>
2015-10-04 18:17:14 -0400

    added off canvas nav

---
+++ app/scripts/main.js
@@ -0,0 +1,22 @@
+$('body').addClass('js');
+
+$('#menu-trigger').on('click', function(e) {
+    e.preventDefault();
+    $('.navigation').toggleClass('off-canvas--hide off-canvas--show');
+    $('.navigation').attr('aria-hidden', 'false');
+});
+
+$('.menu-close').on('click', function() {
+    $('.navigation').toggleClass('off-canvas--hide off-canvas--show');
+    $('.navigation').attr('aria-hidden', 'true');
+});
+
+$(window).resize(function() {
+  if($(window).width() > 640) {
+    $('.navigation').removeClass('off-canvas--show');
+    $('.navigation').attr('aria-hidden', 'false');
+  } else {
+    $('.navigation').addClass('off-canvas--hide');
+    $('.navigation').attr('aria-hidden', 'true');
+  }
+});
