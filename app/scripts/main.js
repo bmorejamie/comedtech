1f86c68e555997180e5c538c5efef2e815a030c9
bmorejamie <bmorejamie@gmail.com>
2015-10-20 22:46:34 -0400

    finishing latest revisions

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -12,36 +12,3 @@ $(function(){
     });
 });
 
-
-
-$('body').addClass('js');
-
-$('#menu-trigger').on('click', function(e) {
-    e.preventDefault();
-    $('.navigation').toggleClass('off-canvas--hide off-canvas--show');
-    $('.navigation').attr('aria-hidden', 'false');
-});
-
-$('.menu-close').on('click', function() {
-    $('.navigation').toggleClass('off-canvas--hide off-canvas--show');
-    $('.navigation').attr('aria-hidden', 'true');
-});
-
-$(window).resize(function() {
-  if($(window).width() > 640) {
-    $('.navigation').removeClass('off-canvas--show');
-    $('.navigation').attr('aria-hidden', 'false');
-  } else {
-    $('.navigation').addClass('off-canvas--hide');
-    $('.navigation').attr('aria-hidden', 'true');
-  }
-});
-
-$(window).scroll(function() {
-    if ($(this).scrollTop() > 1){  
-        $('navbar').addClass("sticky");
-    }
-    else{
-        $('navbar').removeClass("sticky");
-    }
-});
